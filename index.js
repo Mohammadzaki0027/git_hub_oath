37453ab64c908abf3531146ab960554a198f8ada
Mohammadzaki0027 <zaki7875@gmail.com>
2022-10-30 20:15:25 +0530

    oathsetup

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express=require("express")
+const express = require("express");
 
-const app=express()
+const app = express();
 
-app.listen(8080,()=>{
-    try{
-        console.log("connected");
-    }catch(err)
-    {
-        console.log(err);
-    }
-})
+app.listen(8080, () => {
+  try {
+    console.log("connected");
+  } catch (err) {
+    console.log(err);
+  }
+});
