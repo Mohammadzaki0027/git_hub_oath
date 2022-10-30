fcd089717918bb549afc1a249521ab7cc48566dd
Mohammadzaki0027 <zaki7875@gmail.com>
2022-10-30 20:17:42 +0530

    oathsetup

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const bcrypt=require("bcrypt")
 app.get("/",(req,res)=>{
     res.send("welcome to Homepage")
 })
+
+
+app.post("/login",(req,res)=>{
+    res.send("welcome to Homepage")
+})
 app.listen(8080, () => {
   try {
     console.log("connected");
