6c0ac913690e889f2c05384cf7459bd2f6cf18f0
Mohammadzaki0027 <zaki7875@gmail.com>
2022-11-01 13:34:31 +0530

    somesetup

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const express = require("express");
 const app = express();
-app.use(express.json())
-const axios=require("axios")
+app.use(express.json());
+const axios = require("axios");
 
-app.get("/",(req,res)=>{
-   return  res.sendFile(__dirname+"/index.html")
-})
-const client_secret="5bca7e9756391322c75f34f024a167b4e7007930"
-const client_id="d9ef25dcf93821d24201"
+app.get("/", (req, res) => {
+  return res.sendFile(__dirname + "/index.html");
+});
+const client_secret = "5bca7e9756391322c75f34f024a167b4e7007930";
+const client_id = "d9ef25dcf93821d24201";
 
+app.get("/github/callback", async (req, res) => {
+  const code = req.query.code;
+  const access_token = await axios.post(
+    `https://github.com/login/oauth/access_token`,null,
+    {
+      params: {
+        client_id: client_id,
+        client_secret: client_secret,
+        code: code,
+      },
+      headers:{accept:"application/json"}
+    }
+  );
 
-app.get("/github/callback",(req,res)=>{
-  const code=req.query.code
-const access_token=axios.post(`https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`)
+  // Get userdata
 
-
-// Get userdata
-
-const userdata=axios.get("https://api.github.com/user",{
-  headers:{Authorization:`Bearer ${access_token}`
-}
-})
-  res.send("signin with git hub")
-})
+  const userdata = await axios.get("https://api.github.com/user", {
+    headers: { Authorization: `Bearer ${access_token.data.access_token}` },
+  });
+  console.log(userdata);
+  res.send("signin with git hub");
+});
 
 app.listen(8080, () => {
   try {
